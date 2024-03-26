5af5f3ec9ce67996053f4ee74947392a5a3c8c36
Neha Sahu <1802.nehasahu@gmail.com>
2024-03-26 13:00:46 +0530

    Shipping-UI

--- apparel-ecommerce/src/App.tsx
+++ apparel-ecommerce/src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./components/header";
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
+const Shipping = lazy(() => import("./pages/shipping"));
 
 //Admin route imports.
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -35,6 +36,10 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          {/* { Logged In User Route } */}
+          <Route>
+            <Route path="/shipping" element={<Shipping />} />
+          </Route>
           //Admin Routes
           <Route
           // element={
