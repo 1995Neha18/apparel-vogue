d3822fab04d4d10b5536a890f7104ab735fe0a19
Neha Sahu <1802.nehasahu@gmail.com>
2024-03-27 17:19:33 +0530

    login UI completed

--- apparel-ecommerce/src/App.tsx
+++ apparel-ecommerce/src/App.tsx
@@ -7,6 +7,7 @@ const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
 const Shipping = lazy(() => import("./pages/shipping"));
+const Login = lazy(() => import("./pages/login"));
 
 //Admin route imports.
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -36,6 +37,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          {/* { Not logged In User Route } */}
+          <Route path="/login" element={<Login />} />
           {/* { Logged In User Route } */}
           <Route>
             <Route path="/shipping" element={<Shipping />} />
