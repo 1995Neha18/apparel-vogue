5634feefd6a10f50b09c36bd60b36aee4f7a90f8
Neha Sahu <1802.nehasahu@gmail.com>
2024-03-30 15:05:05 +0530

    search page on progress

--- apparel-ecommerce/src/pages/search.tsx
+++ apparel-ecommerce/src/pages/search.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState } from "react";
 
 const Search = () => {
-  return <div>search</div>;
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Price (High to Low)</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Maxprice: {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+      </aside>
+      <main></main>
+    </div>
+  );
 };
 
 export default Search;
